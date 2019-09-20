a38cc2a9f42b6b8d668c7aea9abba81e73290dcc
LORENLY <lyrol.lorenzo@oocl.com>
2019-09-20 20:40:03 +0800

    UpdategetCurrentMethodName

--- src/test/java/com/cultivation/javaBasic/showYourIntelligence/StackFrameHelper.java
+++ src/test/java/com/cultivation/javaBasic/showYourIntelligence/StackFrameHelper.java
@@ -1,14 +1,13 @@
 package com.cultivation.javaBasic.showYourIntelligence;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class StackFrameHelper {
     public static String getCurrentMethodName() {
         // TODO: please modify the following code to pass the test
         // <--start
-        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
-        int var = stackTraceElements[2].toString().indexOf("(");
-        return stackTraceElements[2].toString().substring(0,var);
+        return Thread.currentThread().getStackTrace()[2].getClassName() + "." + Thread.currentThread().getStackTrace()[2].getMethodName();
         // --end-->
     }
+
+
 }

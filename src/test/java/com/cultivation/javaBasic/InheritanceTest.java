bfd25f7a7b57da0158c0e971e6181cb8c76a623e
LORENLY <lyrol.lorenzo@oocl.com>
2019-09-20 20:51:54 +0800

    updateShould_call_super_class_constructor

--- src/test/java/com/cultivation/javaBasic/InheritanceTest.java
+++ src/test/java/com/cultivation/javaBasic/InheritanceTest.java
@@ -27,7 +27,9 @@ class InheritanceTest {
 
         // TODO: please modify the following code to pass the test
         // <--start
-        final String[] expected = {};
+        SuperClassWithDefaultConstructor s = new SuperClassWithDefaultConstructor();
+
+        final String[] expected = {s.getLogs()[0],instance.getLogs()[1]};
         // --end-->
 
         String[] logs = instance.getLogs();
